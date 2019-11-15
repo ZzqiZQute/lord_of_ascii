441b4a0266fe880a49f25aaee25a86ba05ffbeb2
ZzqiZQute <zhaozq11th@ustbsmartcar.org>
2019-11-15 21:53:09 +0800

    add play

---
+++ everywhere_bad_apple/play.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import configparser
+import time
+import gzip
+
+mpvpath1 = '/usr/bin/mpv'
+mpvpath2 = '/usr/local/bin/mpv'
+mpvpath3 = './mpv'
+hassound = True
+null = open('/dev/null', 'w')
+parser = configparser.ConfigParser()
+parser.read(os.path.join(os.path.dirname(__file__),'./settings.conf'))
+fps = parser['General']['fps']
+interval = 1 / (int(fps))
+width = parser['General']['width']
+height = parser['General']['height']
+# print(fps, width, height)
+print("请将终端大小调至%sx%s,并按回车键继续" % (width, int(height) + 1))
+input()
+os.system('clear')
+if hassound:
+    sound = subprocess.Popen(['/usr/bin/mpv', os.path.join(os.path.dirname(__file__),'./sound.aac')], stdout=null, stderr=null)
+firsttime = time.time()
+while True:
+    os.system('clear')
+    currenttime = time.time()
+    idx = int((currenttime - firsttime) / interval)
+    filepath = os.path.join(os.path.dirname(__file__),'./char_%09d.txt.gz' % idx)
+    if  os.path.exists(filepath):
+        with gzip.open(filepath,'rb') as f:
+            print(f.read().decode('utf-8'))
+    else:
+        break
+    time.sleep(0.01)
